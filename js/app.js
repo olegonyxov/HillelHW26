8d361d3106c6826550658fa2f6e09ad7eec2b12f
olegonyxov <olegonyxov@gmail.com>
2024-04-17 22:10:32 +0300

    222

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-  
-  document.getElementById('alertBtn').addEventListener('click', function() {
-    const alertDiv = document.getElementById('myAlert');
-    if (alertDiv.style.display === 'none') {
-      alertDiv.style.display = 'block';
-    } else {
-      alertDiv.style.display = 'none';
-    }
-  });
-  
+document.getElementById('openModalBtn').addEventListener('click', function() {
+  $('#myModal').modal('show');
+});
 
-  const userBirthday = prompt('Введите дату вашего рождения в формате "YYYY-MM-DD":');
-  
-  if (moment(userBirthday, 'YYYY-MM-DD', true).isValid()) {
-    const formattedBirthday = moment(userBirthday, 'YYYY-MM-DD').format('DD.MM.YYYY');
-    alert('Ваша дата рождения в формате "DD.MM.YYYY": ' + formattedBirthday);
+// Показать или скрыть алерт при нажатии на кнопку
+document.getElementById('alertBtn').addEventListener('click', function() {
+  const alertDiv = document.getElementById('myAlert');
+  if (alertDiv.style.display === 'none') {
+    alertDiv.style.display = 'block';
   } else {
-    alert('Ошибка! Неверный формат даты.');
+    alertDiv.style.display = 'none';
   }
+});
+
+// Получение даты рождения от пользователя и преобразование формата
+const userBirthday = prompt('Введите дату вашего рождения в формате "YYYY-MM-DD":');
+
+if (moment(userBirthday, 'YYYY-MM-DD', true).isValid()) {
+  const formattedBirthday = moment(userBirthday, 'YYYY-MM-DD').format('DD.MM.YYYY');
+  alert('Ваша дата рождения в формате "DD.MM.YYYY": ' + formattedBirthday);
+} else {
+  alert('Ошибка! Неверный формат даты.');
+}
