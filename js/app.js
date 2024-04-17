f598d38fa6a2469aa4562f761389e10bdf743d6f
olegonyxov <olegonyxov@gmail.com>
2024-04-17 22:37:48 +0300

    555

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
-document.getElementById('openModalBtn').addEventListener('click', function() {
-  $('#myModal').modal('show');
-});
-
-document.getElementById('alertBtn').addEventListener('click', function() {
-  const alertDiv = document.getElementById('myAlert');
-  if (alertDiv.style.display === 'none') {
-    alertDiv.style.display = 'block';
-  } else {
-    alertDiv.style.display = 'none';
-  }
-});
-
-const userBirthday = prompt('Введите дату вашего рождения в формате "YYYY-MM-DD":');
-
-if (moment(userBirthday, 'YYYY-MM-DD', true).isValid()) {
-  const formattedBirthday = moment(userBirthday, 'YYYY-MM-DD').format('DD.MM.YYYY');
-  alert('Ваша дата рождения в формате "DD.MM.YYYY": ' + formattedBirthday);
-} else {
-  alert('Ошибка! Неверный формат даты.');
-}
+document.getElementById('checkBirthdayBtn').addEventListener('click', function() {
+    const inputDate = document.getElementById('birthdayInput').value;
+    if (moment(inputDate, 'YYYY-MM-DD', true).isValid()) {
+      const formattedBirthday = moment(inputDate, 'YYYY-MM-DD').format('DD.MM.YYYY');
+      alert('Your birth date in "DD.MM.YYYY" format is : ' + formattedBirthday);
+      document.getElementById('errorAlert').style.display = 'none';
+    } else {
+      document.getElementById('errorAlert').style.display = 'block';
+    }
+  });
+  document.getElementById('openModalBtn').addEventListener('click', function() {
+    $('#myModal').modal('show');
+  });
+  document.getElementById('showAlertBtn').addEventListener('click', function() {
+    const alertDiv = document.getElementById('myAlert');
+    if (alertDiv.style.display === 'none') {
+      alertDiv.style.display = 'block';
+    } else {
+      alertDiv.style.display = 'none';
+    }
+  });
